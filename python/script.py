a146c88f045206e357de7d985eae6117bbcb6b03
dev0Guy <guyarieli17@yahoo.com>
2022-04-24 14:25:01 +0300

    finsh

--- python/script.py
+++ python/script.py
@@ -1,28 +1,40 @@
 import argparse
+import csv
 import time
-import numpy as np 
 from utils import *
-# ======== Creeate Praser ========
-parser = argparse.ArgumentParser()
-parser.add_argument("--observation-path",type=str,required=True)
-parser.add_argument("--system-path",type=str,required=True)
-args = parser.parse_args()
-# ======= Get Params From Parser ======= 
-observation_path: str = args.observation_path
-system_path: str = args.system_path
-# ======= Get OBS List From File =======
-obs_list: list[OBS] = list_from_file(observation_path)
-sys: System = new_sys(system_path)
-# ======= Run On All Observersions =======
-for idx, obs in enumerate(obs_list):
-    now: float = time.time() * 1000
-    stop_time = 2*60*1000
-    elapsed,info = bfs(sys,obs,now,stop_time)
-    diagnostic = len(info)
-    minimal_set = min(map(lambda x: len(x),info))
-    row = (idx+1,diagnostic,minimal_set,elapsed)
-    print(row)
-# data_for_exercises/circuits/Data_Observations/c17_iscas85.obs
-# data_for_exercises/circuits/Data_Systems/c17.sys
 
-#"data_for_exercises/circuits/Data_Systems/74182.sys" "data_for_exercises/circuits/Data_Observations/74182_iscas85.obs"
+if __name__ == "__main__":
+    # ======== Creeate Praser ========
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--observation-path", type=str, required=True)
+    parser.add_argument("--system-path", type=str, required=True)
+    args = parser.parse_args()
+    # ======= Get Params From Parser =======
+    observation_path: str = args.observation_path
+    system_path: str = args.system_path
+    # ======= Get OBS List From File =======
+    obs_list: list[OBS] = list_from_file(observation_path)
+    sys: System = new_sys(system_path)
+    # ======= Run On All Observersions =======
+    with open(f'out/{sys.id}.csv', 'w', encoding='UTF8') as csv_file:
+        fields = ['System Name', 'Observation no.', 'Number of Diagnoses', 'Runtime (ms)']
+        # create the csv writer
+        writer = csv.writer(csv_file)
+        for idx, obs in enumerate(obs_list):
+            now: float = time.time() * 1000
+            stop_time = 2*60*1000
+            elapsed, info = bfs(sys, obs, now, stop_time)
+            diagnostic = len(info)
+            minimal_set = min(map(lambda x: len(x), info))
+            row = [sys.id, idx+1, diagnostic, minimal_set, elapsed]
+            writer.writerow(row)
+    # ======= Run Commands =======
+        # python3 python/script.py --observation-path  "data_for_exercises/circuits/Data_Observations/c17_iscas85.obs" --system-path "data_for_exercises/circuits/Data_Systems/c17.sys"
+        # python3 python/script.py --observation-path  "data_for_exercises/circuits/Data_Observations/74182_iscas85.obs" --system-path "data_for_exercises/circuits/Data_Systems/74182.sys"
+        # python3 python/script.py --observation-path  "data_for_exercises/circuits/Data_Observations/74181_iscas85.obs" --system-path "data_for_exercises/circuits/Data_Systems/74181.sys"
+        # python3 python/script.py --observation-path  "data_for_exercises/circuits/Data_Observations/74182_iscas83.obs" --system-path "data_for_exercises/circuits/Data_Systems/74183.sys"
+
+
+
+
+
--- python/utils.py
+++ python/utils.py
@@ -5,13 +5,47 @@ from collections import namedtuple
 from typing import Callable
 
 
-OBS = namedtuple("OBS", ["id", "sys_id","input","output"]) 
-System = namedtuple("SYS", ["id","gates"])
-GateInfo = namedtuple("GateInfo", ["gate","input","output"])
+OBS = namedtuple("OBS", ["id", "sys_id", "input", "output"])
+System = namedtuple("SYS", ["id", "gates"])
+GateInfo = namedtuple("GateInfo", ["gate", "input", "output"])
 
-def list_from_file(obs_path: str) -> list[OBS]:
+
+class GatesFunction(object):
+    @staticmethod
+    def Inverter(input):
+        return not input[0]
+    
+    @staticmethod
+    def Buffer(input):
+        return input[0]
+    
+    @staticmethod
+    def NAnd(input):
+        return not all(input)
+
+    @staticmethod
+    def And(input):
+        return all(input)
+
+    @staticmethod
+    def NOr(input):
+        return not any(input)
+
+    @staticmethod
+    def Or(input):
+        return any(input)
+
+    @staticmethod
+    def Xor(input):
+        out = 0
+        for t in input:
+            out ^= t
+        return t
+
+
+def list_from_file(obs_path: str):
     def line_to_obs(line: str):
-        line = line.replace('.[]()','').split(',')
+        line: list[str] = line.replace('.[]()', '').split(',')
         sys_id: str = line[0]
         id: str = line[1]
         input: list[bool] = list()
@@ -26,16 +60,17 @@ def list_from_file(obs_path: str) -> list[OBS]:
         output: np.array[bool] = np.array(output)
         return OBS(id, sys_id, input, output)
     
-    with open(obs_path,"r") as obs_file:
-        content: list[str] = obs_file.read().replace(".","").split("\n")
+    with open(obs_path, "r") as obs_file:
+        content: list[str] = obs_file.read().replace(".", "").split("\n")
         obs_list: list[OBS] = list()
         for line in content:
             obs_list.append(line_to_obs(line))
         return obs_list 
 
-def new_sys(sys_path: str) -> System:
-    with open(sys_path,"r") as obs_file:
-        content: list[str] = obs_file.read().replace(".","").replace("[","").replace("]","").replace(" ", "").split("\n")
+
+def new_sys(sys_path: str):
+    with open(sys_path, "r") as obs_file:
+        content: list[str] = obs_file.read().replace(".", "").replace("[", "").replace("]", "").replace(" ", "").split("\n")
         id: str = content[0]
         gates: list[GateInfo] = list()
         content: list[str] = content[3:]
@@ -44,34 +79,35 @@ def new_sys(sys_path: str) -> System:
             if tokens[-1] == "":
                 del tokens[-1]
             gate: Callable = None
-            if len(tokens) ==0 :
+            if len(tokens) == 0:
                 continue
             if tokens[0] == "inverter":
-                gate = lambda x: not x 
+                gate = GatesFunction.Inverter
             elif tokens[0] == "buffer":
-                gate = lambda x: x
+                gate = GatesFunction.Buffer
             elif 'nand' in tokens[0]:
-                gate = lambda x: not all(x)
+                gate = GatesFunction.NAnd
             elif 'and' in tokens[0]:
-                gate = lambda x: all(x)
+                gate = GatesFunction.And
             elif 'nor' in tokens[0]:
-                gate = lambda x: not any(x)
+                gate = GatesFunction.NOr
             elif 'or' in tokens[0]:
-                gate = lambda x: any(x)
+                gate = GatesFunction.Or
             elif 'xor' in tokens[0]:
-                gate = lambda x: x[0] ^ x[1]
-            gate_id: str  = tokens[1]
+                gate = GatesFunction.Xor
+            gate_id: str = tokens[1]
             output: str = tokens[2]
             inputs: list[str] = tokens[3:]
-            gates.append(GateInfo(gate,inputs,output))
-        return System(id,gates)
-    
-def activate(sys: System,input: OBS,change: np.array) -> list[bool]:
+            gates.append(GateInfo(gate, inputs, output))
+        return System(id, gates)
+
+
+def activate(sys: System, input: OBS, change: np.array):
     i: list[bool] = input.input
-    z: np.array[bool] = np.full((len(sys.gates)),False)
-    out: np.array[bool] = np.full((len(input.output)),False)
-    for idx,info in enumerate(sys.gates):
-        gate_input: list[bool] = list(map(lambda x: i[int(x[1:])-1] if 'i' in x else z[int(x[1:])-1],info[1]))
+    z: np.array[bool] = np.full((len(sys.gates)), False)
+    out: np.array[bool] = np.full((len(input.output)), False)
+    for idx, info in enumerate(sys.gates):
+        gate_input: list[bool] = list(map(lambda x: i[int(x[1:])-1] if 'i' in x else z[int(x[1:])-1], info[1]))
         index: int = int(info[2][1:])-1
         activate_out: bool = info[0](gate_input)
         if change[idx]:
@@ -81,16 +117,16 @@ def activate(sys: System,input: OBS,change: np.array) -> list[bool]:
         else:
             z[index] = activate_out
     return out
-    
-def bfs(sys: System,obs: OBS,now: float,stop_time: float) -> tuple[int,list[set]]:
+
+
+def bfs(sys: System, obs: OBS, now: float, stop_time: float) -> tuple[int, list[set]]:
     deqeue: list[set] = list()
     minimal_set: list[set] = list()
     elapsed: float = time.time() * 1000
     # insert all tokens
-    for idx in range(len(sys)):
-        deqeue.append({idx})
-    while len(deqeue) > 0 and (elapsed-now)< stop_time:
-        combination: set = deqeue.pop(0);
+    deqeue = [{idx} for idx in range(len(sys.gates))]
+    while len(deqeue) > 0 and (elapsed-now) < stop_time:
+        combination: set = deqeue.pop(0)
         contains_combination: bool = False
         for set_ in minimal_set:
             if contains_combination: 
@@ -98,18 +134,18 @@ def bfs(sys: System,obs: OBS,now: float,stop_time: float) -> tuple[int,list[set]
             contains_combination |= len(set_.difference(combination)) == 0 
         if contains_combination:
             continue
-        changes: np.array[bool] = np.full((len(sys.gates)),False)
+        changes: np.array[bool] = np.full((len(sys.gates)), False)
         for val in combination:
             changes[val] = True
-        out: list[bool] = activate(sys,obs,changes)
-        matching: bool = all(map(lambda x: x[0] == x[1],zip(out,obs.output)))
+        out: list[bool] = activate(sys, obs, changes)
+        matching: bool = all(map(lambda x: x[0] == x[1], zip(out, obs.output)))
         if matching:
             minimal_set.append(combination);
         else:
-            for idx in range(0,len(sys.gates)):
+            for idx in range(0, len(sys.gates)):
                 if not(idx in combination):
                     x: set = combination.copy()
                     x.add(idx)
                     deqeue.append(x)
         elapsed: float = time.time() * 1000
-    return (elapsed-now,minimal_set)
+    return (elapsed-now, minimal_set)
